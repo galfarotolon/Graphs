86afc25285e700c528e896a60ad868b2627a4c53
Guillermo Alfaro <galfarotolon@gmail.com>
2020-08-11 18:01:10 -0500

    ancestor kinda works, still fixing some bugs

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,32 @@
+from util import Queue, Stack, Graph
+
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    graph = Graph()
+
+    for pair in ancestors:
+        graph.add_vertex(pair[0])
+        graph.add_vertex(pair[1])
+
+        graph.add_edge(pair[1], pair[0])
+    
+    q = Queue()
+
+    q.enqueue([starting_node])
+    max_path_len = 1
+    earliest_ancestor = -1
+
+    while q.size() > 0:
+        path = q.dequeue()
+        v = path[-1]
+
+        if (len(path) >= max_path_len and v < earliest_ancestor) or (len(path) > max_path_len):
+            earliest_ancestor = v
+            max_path_len = len(path)
+        for neighbor in graph.vertices[v]:
+            path_copy = list(path)
+            path_copy.append(neighbor)
+            q.enqueue(path_copy)
+
+    return earliest_ancestor
+
